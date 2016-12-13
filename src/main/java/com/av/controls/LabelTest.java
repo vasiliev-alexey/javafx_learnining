defedf08348a9b15240282c9e13f61ea591b9724
vasiliev-alexey <alex_v@nomail.ru>
2016-12-13 16:44:32 +0300

    +  controls

---
+++ src/main/java/com/av/controls/LabelTest.java
@@ -0,0 +1,38 @@
+package com.av.controls;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ * Created by alexey on 13.12.16.
+ */
+public class LabelTest extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        TextField fNameFld = new TextField();
+        Label fNameLbl = new Label("_First Name:");
+        fNameLbl.setLabelFor(fNameFld);
+        fNameLbl.setMnemonicParsing(true);
+        TextField lNameFld = new TextField();
+        Label lNameLbl = new Label("_Last Name:");
+        lNameLbl.setLabelFor(lNameFld);
+        lNameLbl.setMnemonicParsing(true);
+        GridPane root = new GridPane();
+        root.addRow(0, fNameLbl, fNameFld);
+        root.addRow(1, lNameLbl, lNameFld);
+        root.setStyle("-fx-padding: 10;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 2;" +
+                "-fx-border-insets: 5;" +
+                "-fx-border-radius: 5;" +
+                "-fx-border-color: blue;");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Using Labels");
+        stage.show();
+    }
+}
---
+++ src/main/java/com/av/controls/PersonStringConverter.java
@@ -0,0 +1,35 @@
+package com.av.controls;
+
+import com.av.domain.Person;
+import javafx.util.StringConverter;
+
+/**
+ * Created by alexey on 13.12.16.
+ */
+public class PersonStringConverter  extends StringConverter<Person> {
+
+
+        @Override
+        public String toString(Person p) {
+            return p == null? null : p.getLastName() + ", " + p.getFirstName();
+        }
+        @Override
+        public Person fromString(String string) {
+            Person p = null;
+            if (string == null) {
+
+                return p;
+            }
+            int commaIndex = string.indexOf(",");
+            if (commaIndex == -1) {
+// Treat the string as first name
+                p = new Person(string, null, null);
+            } else {
+                // Ignoring string bounds check for brevity
+                String firstName = string.substring(commaIndex + 2);
+                String lastName = string.substring(0, commaIndex);
+                p = new Person(firstName, lastName, null);
+            }
+            return p;
+            }
+}
